e554ae1b28eed246bfc5547a62676a9e9cb9942b
유성은 <yuseong-eun@yuseong-eun-ui-MacBookPro.local>
2020-09-22 00:15:33 +0900

    state end

--- src/App.js
+++ src/App.js
@@ -1,57 +1,25 @@
 import React from 'react';
 import PropTypes from "prop-types";
 
+class App extends React.Component{
+  state = {
+    isLoading: true
+  };
 
-const foodILike = [
-  {
-    id: 1,
-    name: "kimchi",
-    taste: "50",
-    rating: 5
-  },
-  {
-    id: 2,
-    name: "potato",
-    taste: "10",
-    rating: 4.7
-  },
-  {
-    id: 3,
-    name: "apple",
-    taste: "90",
-    rating: 4.1
-  },
-  {
-    id: 4,
-    name: "banana",
-    taste: "100",
-    rating: 4.3
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 5000);
   }
-];
 
-Food.prototype = {
-  name: PropTypes.string.isRequired,
-  taste: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-};
-
-function Food({name, taste, rating}){
-  return (
-    <div>
-      <h3>I likn {name}. Taste Score is {taste}.</h3>
-      <h5>{rating}/5.0</h5>
-    </div>
-  );
-}
-
-function App() {
-  return (
-    <div>
-      {foodILike.map(dish => (
-        <Food key={dish.id} name={dish.name} taste={dish.taste} rating={dish.rating}/>
-      ))}
-    </div>
-  );
+  render(){
+    const { isLoading } = this.state;
+    return(
+      <div>
+        {isLoading ? "Loading.." : "we are ready."}
+      </div>
+    )
+  }
 }
 
 export default App;
