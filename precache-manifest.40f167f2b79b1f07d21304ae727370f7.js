83faa9363b1ab892ab10c7c774989324de990d2e
유성은 <yuseong-eun@yuseong-eun-ui-MacBookPro.local>
2020-09-24 22:48:03 +0900

    Updates

---
+++ precache-manifest.40f167f2b79b1f07d21304ae727370f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13239e5a803b9117779f4e6260521975",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "7f46e75d8a0f966283a4",
+    "url": "/movie_app_2020/static/css/main.47afaf7d.chunk.css"
+  },
+  {
+    "revision": "11748d6dc8da0fe94e79",
+    "url": "/movie_app_2020/static/js/2.8f3b569d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.8f3b569d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f46e75d8a0f966283a4",
+    "url": "/movie_app_2020/static/js/main.087f8ee7.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
